52da9a85958517bc6d4e3325975d2678d4f7d416
Lindsey1d <lindseynweb@gmail.com>
2016-11-03 15:56:02 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+var m= 16;
+var s=17;
+
+var m =prompt("5+4");
+if (m==9){
+confirm("yay you got it right");
+}
+else{
+confirm("you're bad at math");
+}
+function math() {
+	var answer = prompt("Enter 1+1:");
+	if (answer == 2) {
+		confirm("You got it right.Your answer was" + answer);
+   }
+   else {
+   confirm("You got it wrong");
+	}
+}
